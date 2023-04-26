6ab691d33cc766095e5c492afed13c52fc24ba94
gabi-rogers23 <gabirogers23@gmail.com>
2023-04-26 15:40:01 -0500

    subpage V1 with icons below title

--- src/components/environmentalDianostics.js
+++ src/components/environmentalDianostics.js
@@ -1,11 +1,32 @@
 import React from "react";
 import { NavBar, Footer } from "./exports";
+import { GiMushrooms } from "react-icons/gi";
 
 const Environmental = () => {
   return (
     <>
       <NavBar />
-      <div></div>
+      <div className="subContainer">
+        <div className="title">Environmental Diagnostics</div>
+        <div className="iconContainer">
+          <div className="circle">
+            <GiMushrooms className="person" />
+          </div>
+          <div className="circle">
+            <GiMushrooms className="person" />
+          </div>
+        </div>
+        <div className="description">
+          Trace Diagnostics offers development services in the environmental
+          industry. For the detection of analytes in order to determine air
+          quality, contamination of surfaces with microorganisms, contamination
+          with chemical residues, etc., Trace Diagnostics can serve to identify
+          rare reagents and perform development into viable products, products
+          that can lead to inexpensive and rapid diagnosis to ensure immediate
+          and proper response to ensure safety and quality in all environmental
+          settings.
+        </div>
+      </div>
       <Footer />
     </>
   );
