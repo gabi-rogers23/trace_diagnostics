937d0ec993fa3de918e5da2e762e9e036030dfbc
gabi-rogers23 <gabirogers23@gmail.com>
2023-04-26 21:59:09 -0500

    Environmental Diagnostics design complete

--- src/components/environmentalDianostics.js
+++ src/components/environmentalDianostics.js
@@ -1,17 +1,27 @@
 import React from "react";
 import { NavBar, Footer } from "./exports";
-import { GiMushrooms } from "react-icons/gi";
+import { GiMushrooms, GiChemicalDrop } from "react-icons/gi";
+import {MdAir} from "react-icons/md"
 
 const Environmental = () => {
   return (
     <div className="mainContainer">
       <NavBar />
       <div className="subContainer">
-      <div className="iconContainer">
+      <div className="subImage"></div>
+      <div className="titleContainer">
+        <div className="iconContainer">
+      <div className="circle">
+              <GiMushrooms className="person" />
+            </div>
+            <div className="circle">
+              <MdAir className="person" />
+            </div> 
+            <div className="circle">
+              <GiChemicalDrop className="person" />
+            </div> 
+            </div>
       <div className="title">Environmental Diagnostics</div>
-        <div className="subImage"></div>
-        </div>
-
         <div className="description">
           Trace Diagnostics offers development services in the environmental
           industry. For the detection of analytes in order to determine air
@@ -22,6 +32,8 @@ const Environmental = () => {
           and proper response to ensure safety and quality in all environmental
           settings.
         </div>
+        </div>
+
       </div>
       <Footer />
     </div>
