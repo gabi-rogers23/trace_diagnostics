d45ab1c548d0c7b3852d5116fa77ea9f119607a5
gabi-rogers23 <gabirogers23@gmail.com>
2023-05-09 11:16:26 -0500

    updated nav logo to go to home page

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,10 +1,15 @@
 import React from "react";
 import NavDrawer from "./navDrawer";
+import { useNavigate } from "react-router-dom";
 
 const NavBar = () => {
+  const navigate = useNavigate()
   return (
     <div className="navBarContainer">
-      <div className="logo">TRACE DIAGNOSTICS, INC.</div>
+      <div className="logo" onClick={((e)=>{
+        e.preventDefault();
+        navigate("/")
+      })}>TRACE DIAGNOSTICS, INC.</div>
       <div><NavDrawer/></div>
     </div>
   );
