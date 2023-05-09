ccd6707167e9586ec77f626d6a3d253d2cb45183
gabi-rogers23 <gabirogers23@gmail.com>
2023-05-09 15:10:40 -0500

    cleaned up code in environmental diagnostics

--- src/components/environmentalDianostics.js
+++ src/components/environmentalDianostics.js
@@ -1,40 +1,49 @@
 import React from "react";
 import { NavBar, Footer } from "./exports";
 import { GiMushrooms, GiChemicalDrop } from "react-icons/gi";
-import {MdAir} from "react-icons/md"
+import { MdAir } from "react-icons/md";
 
 const Environmental = () => {
   return (
     <div className="mainContainer">
       <NavBar />
       <div className="subContainer">
-        <div className="subImage"><div className="title">Environmental Diagnostics</div></div>
-      <div className="titleContainer">
-        <div className="iconContainer">
-      <div className="circle">
+        <div className="subImage">
+          <div className="title">Environmental Diagnostics</div>
+        </div>
+        <div className="content">
+          <div className="iconContainer">
+            <div className="circle">
               <GiMushrooms className="person" />
             </div>
             <div className="circle">
               <MdAir className="person" />
-            </div> 
+            </div>
             <div className="circle">
               <GiChemicalDrop className="person" />
-            </div> 
             </div>
-        <div className="description">
-          Trace Diagnostics offers development services in the environmental
-          industry. For the detection of analytes in order to determine air
-          quality, contamination of surfaces with microorganisms, contamination
-          with chemical residues, etc., Trace Diagnostics can serve to identify
-          rare reagents and perform development into viable products, products
-          that can lead to inexpensive and rapid diagnosis to ensure immediate
-          and proper response to ensure safety and quality in all environmental
-          settings.
-        </div>
+          </div>
+          <div className="description">
+            Trace Diagnostics offers development services in the environmental
+            industry. For the detection of analytes in order to determine air
+            quality, contamination of surfaces with microorganisms,
+            contamination with chemical residues, etc., Trace Diagnostics can
+            serve to identify rare reagents and perform development into viable
+            products, products that can lead to inexpensive and rapid diagnosis
+            to ensure immediate and proper response to ensure safety and quality
+            in all environmental settings.
+          </div>
         </div>
       </div>
-        <div className="credit">Photo by: <a href="https://unsplash.com/@gblokker?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"  target="_blank">Guido Blokker.</a>
-        </div>
+      <div className="credit">
+        Photo by:{" "}
+        <a
+          href="https://unsplash.com/@gblokker?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+          target="_blank"
+        >
+          Guido Blokker.
+        </a>
+      </div>
       <Footer />
     </div>
   );
