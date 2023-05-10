622a77ada9cf6bca682fda714015a41513e41048
gabi-rogers23 <gabirogers23@gmail.com>
2023-05-10 11:25:57 -0500

    Contact WIP

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,58 +1,62 @@
 import React from "react";
 import { NavBar, Footer } from "./exports";
-import { BsBoxFill, BsClipboardCheckFill } from "react-icons/bs";
+import { AiTwotonePhone } from "react-icons/ai";
 import { FaMapMarkerAlt } from "react-icons/fa";
-const img = require("../assets/Technical_Compliance_03.jpg");
+import { MdEmail, MdManageAccounts } from "react-icons/md";
 
 const Contact = () => {
   return (
     <div className="mainContainer">
       <NavBar />
       <div className="subContainer">
-        <div className="subImageTechnical">
+        <div className="subImageContact">
           <div className="title">Contact Us</div>
         </div>
-        <div className="content">
-          <div className="iconContainer">
+        <div className="contentContact">
+          <div className="contactIcon">
             <div className="circle">
               <FaMapMarkerAlt className="person" />
             </div>
-              <span>Trace Diagnostics, Inc. 830 Seton Court, Suite 12 Wheeling, IL 60090</span>
+            <span>
+              Trace Diagnostics, Inc.
+              <br /> 830 Seton Court, Suite 12 Wheeling, IL 60090
+            </span>
           </div>
-
-          <div className="iconContainer">
+          <div className="contactIcon">
             <div className="circle">
-              <BsBoxFill className="person" />
+              <AiTwotonePhone className="person" />
             </div>
-              <span>Phone: 847-9-TRACE-9</span>
+            <span>
+              <b>Phone: </b>847-9-TRACE-9 (+1 847-987-2239)
+            </span>
           </div>
-
-          <div className="iconContainer">
+          <div className="contactIcon">
             <div className="circle">
-              <BsBoxFill className="person" />
+              <MdEmail className="person" />
             </div>
-              <span>E-Mail: service@tracedx.com</span>
+            <span>
+              <b>E-Mail: </b>
+              <a href="mailto:service@tracedx.com">service@tracedx.com</a>
+            </span>
           </div>
-
-          <div className="iconContainer">
+          <div className="contactIcon">
             <div className="circle">
-              <BsBoxFill className="person" />
+              <MdManageAccounts className="person" />
             </div>
-              <span>Accounts Payable
-            and Billing: Trace Diagnostics, Inc. 2033 N. Milwaukee Ave., Suite
-            120 Riverwoods, IL 60015</span>
+            <span>
+              <b>Accounts Payable and Billing: </b>Trace Diagnostics, Inc.
+              <br /> 2033 N. Milwaukee Ave., Suite 120 Riverwoods, IL 60015
+            </span>
           </div>
 
-          <div className="quote">
-          </div>
-          <div className="description">
-          </div>
+          <div className="quote"></div>
+          <div className="description"></div>
         </div>
       </div>
       <div className="credit">
         Photo by:{" "}
-        <a href="https://unsplash.com/@romaindancre?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
-          Romain Dancre
+        <a href="https://unsplash.com/@nasa?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+          NASA
         </a>
       </div>
       <Footer />
